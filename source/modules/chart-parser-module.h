4c39d0f2dd74e6022b6c776153a328a9c864d45c
AncuL001 <ancul001@gmail.com>
2021-06-24 23:06:59 +0700

    implement exporting

--- source/modules/chart-parser-module.h
+++ source/modules/chart-parser-module.h
@@ -7,11 +7,17 @@
 class ChartParserModule : public Module
 {
 public:
-
-	Chart* ParseAndGenerateChartSet(std::string InPath);
+	void SetCurrentChartPath(const std::string InPath);
+	Chart* ParseAndGenerateChartSet(std::string _CurrentChartPath);
+	void ExportChartSet(Chart* InChart, const Time InSongLength);
 
 private:
 
+	std::string _CurrentChartPath;
+
 	Chart* ParseChartOsuImpl(std::ifstream& InIfstream, std::string InPath);
 	Chart* ParseChartStepmaniaImpl(std::ifstream& InIfstream, std::string InPath);
+
+	void ExportChartOsuImpl(Chart* InChart, const Time InSongLength, std::ofstream& InOfStream);
+	void ExportChartStepmaniaImpl(Chart* InChart, const Time InSongLength, std::ofstream& InOfStream);
 };
